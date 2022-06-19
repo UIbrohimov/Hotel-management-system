a6c43b37288bcec7e2d1cadaaa2069cec866c310
Ubaydullo <ubuhobbit@gmail.com>
2022-06-20 01:07:52 +0500

    soemthong

--- nest4guest/urls.py
+++ nest4guest/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('main.urls', 'main'), namespace='main')),
-    path('payment/', include(('payments.urls', 'payments'), namespace='payments')),
     path('reservations/', include(('reservations.urls', 'reservations'), namespace='reservations')),
     path('userinfo/', include(('userinfo.urls', 'userinfo'), namespace='userinfo')),
 ]
